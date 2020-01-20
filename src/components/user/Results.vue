b821ddee47ac496aa67e6dc87b8eb4d0eb50a389
ILES Matthew <miles@one.steria.dom>
2020-01-20 09:02:27 +0000

    Results page

--- src/components/user/Results.vue
+++ src/components/user/Results.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="quiz">
-    <div class="text">
-        RESULTS
+    <div class="box">
+        <div class="text">
+            RESULTS
+        </div>
+
+        <div class="score" id="Score"> </div> <div class="score1"> /10</div>
+
+        <button class="back" @click="back_to_home"> Back </button>
+
     </div>
-
-    <div class="score" id="Score"> </div> <div class="score"> /10</div>
-
-
-
   </div>
 </template>
 
@@ -20,7 +22,9 @@ export default {
     };
   },
   methods:{
-
+    back_to_home:function(){
+        this.$router.push('Home');
+    }
   },
   mounted() {
     if (localStorage.Score) {
@@ -39,18 +43,40 @@ export default {
 
 <style scoped>
 .quiz {
-  background-image: url('~@/assets/images/questionmark.jpeg');
-  width: 100%;
-  height: 800px;
-  background-size: 100%;
+    background-image: url('~@/assets/images/treasurehunt.gif');
+    width: 100%;
+    height: 800px;
+    background-size: 100%;
+}
+.box{
+    position: fixed;
+    top:5%;
+    left:10%;
+    bottom:10%;
+    right:10%;
+    background:rgba(100, 97, 97, 0.9);
+    border-style: solid;
+    border-width: 4px;
+    border-color: white;
 }
 .text{
-    font-size: 10vh;
-    color: yellow;
+    font-size: 9vh;
+    color: white;
+    top:10px;
 }
 
 .score{
     font-size: 10vh;
-    color: red;
+    color: white;
+}
+.score1{
+    font-size: 10vh;
+    color: rgb(0, 0, 0);
+}
+.back{
+    font-size: 4vh;
+    position: fixed;
+    margin-left:-9%;
+    bottom:25%;
 }
 </style>
